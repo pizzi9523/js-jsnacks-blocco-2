df2519579ed0f5dca2c561575ec4b6263e9cfa24
pizzi9523 <pizzitolagiuseppe@live.it>
2021-10-25 12:18:32 +0200

    Creo array di squadre di calcio

---
+++ js-snack-2/main.js
@@ -0,0 +1,38 @@
+const teams = [
+    {
+        nome: "Milan",
+        punti_fatti: 0,
+        falli_subiti: 0
+    },
+
+    {
+        nome: "Inter",
+        punti_fatti: 0,
+        falli_subiti: 0
+    },
+
+    {
+        nome: "Juventus",
+        punti_fatti: 0,
+        falli_subiti: 0
+    },
+
+    {
+        nome: "Roma",
+        punti_fatti: 0,
+        falli_subiti: 0
+    },
+
+    {
+        nome: "Fiorentina",
+        punti_fatti: 0,
+        falli_subiti: 0
+    },
+]
+
+
+
+
+
+
+
